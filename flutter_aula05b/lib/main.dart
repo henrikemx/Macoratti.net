a2bffc9b6223eaee4450c0ec38ed495931cef5f8
henrikemx <hmaxmacedo@gmail.com>
2020-01-28 17:20:21 -0300

    Projeto gerado em 28/01/2020

---
+++ flutter_aula05b/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Curso de Flutter - Macoratti.net',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
