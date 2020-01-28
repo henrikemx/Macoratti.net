a536a58574d3c03fa3ebade2986d2a69a43adac5
henrikemx <hmaxmacedo@gmail.com>
2020-01-28 15:00:23 -0300

    Atualizado às 15:00 hs de 28/01/2020

--- flutter_aula03/lib/main.dart
+++ flutter_aula03/lib/main.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MyApp()  
-);
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text('Olá Flutter 2020',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold
-      )
-      )
-    ,);
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    );
   }
 }
-
